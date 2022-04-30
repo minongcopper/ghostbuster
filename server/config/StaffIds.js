38ec530d534e0a20e83ada726b7c2eaf057463e8
Jaimie Diemer <pbandjaimie@gmail.com>
2022-04-30 12:22:00 -0700

    Bill's Changes - TPs fixed!

---
+++ server/config/StaffIds.js
@@ -0,0 +1,7 @@
+export const staffIdMap = {
+  '5e5fc28e4c059b78851401d8': 'Magee Mooney Caliguire',
+  '607efbd90e52f3592e99fa3e': 'Michelle Lockett',
+  '61707d4909365066e555d94a': 'Debalina Mukherjee'
+};
+
+// @note:Add trello user ids here to be excluded from reporting filters
